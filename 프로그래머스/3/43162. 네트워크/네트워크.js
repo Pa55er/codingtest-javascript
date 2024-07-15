77549ad9012523180c114c7359354073ca1317df
yunseokjun <57414420+Pa55er@users.noreply.github.com>
2024-07-15 21:30:36 +0900

    [level 3] Title: 네트워크, Time: 0.47 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,20 @@
+function solution(n, computers) {
+    const dfs = (a) => {
+        for(let j = 0; j < n; j++) {
+            if(a != j && computers[a][j] == 1 && visited[j] == false) {
+                visited[j] = true;
+                dfs(j);
+            }
+        }
+    };
+    
+    var answer = 0;
+    let visited = new Array(n).fill(false);
+    for(let i = 0; i < n; i++) {
+        if(visited[i] == true) continue;
+        visited[i] = true;
+        answer++;
+        dfs(i);
+    }
+    return answer;
+}
