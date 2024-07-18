e6e788914a5d4f5430be94a1cb2730ee0f3a8be6
yunseokjun <57414420+Pa55er@users.noreply.github.com>
2024-07-18 09:59:52 +0900

    [level 3] Title: 최고의 집합, Time: 0.52 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
@@ -0,0 +1,18 @@
+function solution(n, s) {
+    var answer = [];
+    let a = Math.floor(s / n);
+    let b = s % n;
+    if(a == 0) return [-1];
+    for(let i = 0; i < n; i++) {
+        if(i < n - b) {
+            answer.push(a);
+        }
+        else {
+            answer.push(a + 1);
+        }
+    }
+    return answer;
+}
+
+debugger;
+solution(2,9);
