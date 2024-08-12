d4d48df059a5fdfae3eda6ca9299b125a98cca5d
yunseokjun <57414420+Pa55er@users.noreply.github.com>
2024-08-12 21:22:44 +0900

    [level 2] Title: 기능개발, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,25 +1,16 @@
 function solution(progresses, speeds) {
     var answer = [];
     let days = progresses.map((a, i) => {return Math.ceil((100 - a) / speeds[i])});
-    let count;
-    for(let i = 0; i < days.length; i++) {
-        if(i == days.length - 1) {
-            if(days[i] > 0) {
-                answer.push(count);
-                answer.push(1);
-            }
-            else {
-                answer.push(count + 1);
-            }
-        }
-        else if(i == 0 || days[i] > 0) {
-            for(let j = i + 1; j < days.length; j++) {
-                days[j] -= days[i];
-            }
-            if(i != 0) answer.push(count);
+    let count = 1;
+    let target = days[0];
+    for(let i = 1; i < days.length; i++) {
+        if(target >= days[i]) count++;
+        else {
+            answer.push(count);
             count = 1;
+            target = days[i];
         }
-        else count++;
+        if(i === days.length - 1) answer.push(count);
     }
     return answer;
 }
