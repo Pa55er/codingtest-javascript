6e69b87b0b41361b402ba9cbd5f892ca9b596838
yunseokjun <57414420+Pa55er@users.noreply.github.com>
2024-08-17 03:59:08 +0900

    [level 2] Title: 예상 대진표, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,9 +1,7 @@
 function solution(n,a,b) {
-    var answer = 1;
+    var answer = 0;
     a--;
     b--;
-    a = Math.floor(a / 2);
-    b = Math.floor(b / 2);
     while(a != b) {
         answer++;
         a = Math.floor(a / 2);
