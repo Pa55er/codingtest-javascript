02e6aab5270710633353be48d56b405134c64ff1
yunseokjun <57414420+Pa55er@users.noreply.github.com>
2024-08-26 23:02:09 +0900

    [level 2] Title: 피로도, Time: 5.20 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -13,12 +13,6 @@ function solution(k, dungeons) {
     
     var answer = 0;
     const visited = new Array(dungeons.length).fill(false);
-    for(let i = 0; i < dungeons.length; i++) {
-        if(k >= dungeons[i][0]) {
-            visited[i] = true;
-            dfs(1, k - dungeons[i][1]);
-            visited[i] = false;
-        }
-    }
+    dfs(0, k);
     return answer;
 }
