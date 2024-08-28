bb185828d9f1cb99071f4299d905ad4f8b78ce62
yunseokjun <57414420+Pa55er@users.noreply.github.com>
2024-08-28 20:47:44 +0900

    [level 2] Title: 가장 큰 수, Time: 218.57 ms, Memory: 44.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+    var answer;
+    answer = numbers.map((a) => String(a)).sort((a,b) => (b + a) - (a + b)).join('');
+    if(answer[0] == '0') answer = '0';
+    return answer;
+}
