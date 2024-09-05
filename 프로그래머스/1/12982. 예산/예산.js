da9688d75e778b445ba577eac1c958f096f9b429
yunseokjun <57414420+Pa55er@users.noreply.github.com>
2024-09-05 21:53:04 +0900

    [level 1] Title: 예산, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,12 @@
+function solution(d, budget) {
+    d.sort((a, b) => a - b);
+    let answer = 0;
+    for (let i = 0; i < d.length; i++) {
+        if (budget < d[i]) break;
+        else {
+            budget -= d[i];
+            answer++;
+        }
+    }
+    return answer;
+}
