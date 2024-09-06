c56ab428580a7cccfd5a08d3dfd0e25d8e43ae40
yunseokjun <57414420+Pa55er@users.noreply.github.com>
2024-09-06 12:40:21 +0900

    [level 2] Title: 짝지어 제거하기, Time: 64.43 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,12 +1,10 @@
-function solution(s)
-{
-    var answer = 0;
-    let target = [];
+function solution(s) {
+    let answer = 0;
+    const target = [];
     for(let i = 0; i < s.length; i++) {
-        if(target.length == 0) target.push(s[i]);
-        else if(target[target.length - 1] == s[i]) target.pop();
+        if(target[target.length - 1] === s[i]) target.pop();
         else target.push(s[i]);
     }
-    if(target.length == 0) answer = 1;
+    if(target.length === 0) answer = 1;
     return answer;
 }
