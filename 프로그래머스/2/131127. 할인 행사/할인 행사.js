2916a6e050dae388083b65fc69985770a8a0c8a5
yunseokjun <57414420+Pa55er@users.noreply.github.com>
2024-09-13 01:41:22 +0900

    [level 2] Title: 할인 행사, Time: 45.53 ms, Memory: 41.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,23 @@
+function solution(want, number, discount) {
+    var answer = 0;
+    for(let i = 0; i <= discount.length - 10; i++) {
+        const m = new Map();
+        for(let i = 0; i < want.length; i++) {
+            m[want[i]] = number[i];
+        }
+        let flag = 0;
+        for(let j = 0; j < 10; j++) {
+            let a = discount[i + j];
+            if(m[a]) {
+                m[discount[i + j]]--;
+                if(m[discount[i + j]] == 0) m.delete(discount[i + j]);
+            }
+            else {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0 && m.size == 0) answer++;
+    }
+    return answer;
+}
