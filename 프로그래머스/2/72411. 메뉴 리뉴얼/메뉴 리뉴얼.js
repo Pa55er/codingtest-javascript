f47bf64e050f96156fd259da7d9f3dc04bc42340
yunseokjun <57414420+Pa55er@users.noreply.github.com>
2024-09-13 11:31:16 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 22.26 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -0,0 +1,29 @@
+function solution(orders, course) {
+    const courseCount = new Map();
+    const maxCount = new Array(11).fill(0);
+    let target;
+    
+    const backTracking = (index, len, str) => {
+        if(len === 0) {
+            if(courseCount.has(str)) {
+                courseCount.set(str, courseCount.get(str) + 1);
+                maxCount[str.length] = Math.max(maxCount[str.length], courseCount.get(str));
+            } else {
+                courseCount.set(str, 1);
+            }
+        }
+        for(let i = index; i < target.length; i++) {
+            backTracking(i + 1, len - 1, str + target[i]);
+        }
+    }
+    
+    for(const len of course) {
+        for(target of orders) {
+            target = target.split("").sort();
+            backTracking(0, len, "");
+        }
+    }
+    return Array.from(courseCount.keys())
+        .filter(course => courseCount.get(course) === maxCount[course.length])
+        .sort();
+}
