abb49c74ad94507f076800dc32cd2a4148feecfc
Pa55er <57414420+Pa55er@users.noreply.github.com>
2024-10-11 02:21:36 +0900

    [level 2] Title: 롤케이크 자르기, Time: 125.66 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -3,12 +3,10 @@ function solution(topping) {
     const a = new Map();
     const b = new Map();
     for(let i = 0; i < topping.length; i++) {
-        if(b.has(topping[i])) b.set(topping[i], b.get(topping[i]) + 1);
-        else b.set(topping[i], 1);
+        b.set(topping[i], (b.get(topping[i]) || 0) + 1);
     }
     for(let i = 0; i < topping.length; i++) {
-        if(a.has(topping[i])) a.set(topping[i], a.get(topping[i]) + 1);
-        else a.set(topping[i], 1);
+        a.set(topping[i], (a.get(topping[i]) || 0) + 1);
         b.set(topping[i], b.get(topping[i]) - 1);
         if(b.get(topping[i]) == 0) b.delete(topping[i]);
         if(a.size == b.size) answer++;
